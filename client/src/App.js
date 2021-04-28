d33ffc4400d617850cd360c3d6eaaf5d1462b68e
HanYi <nghanyi1997@gmail.com>
2021-04-28 17:13:17 +0800

    Removed typo from header

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          Memoriesss
+          Memories
         </Typography>
         <img
           className={classes.image}
